7afd986e388dd86191939ff2099bf7fb78cb17f6
pll <pll@pll-macbookpro.attlocal.net>
2018-07-14 19:14:54 -0700

    Initial Commit

---
+++ StatusBarNotes/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  StatusBarNotes
+//
+//  Created by Filippo Pellolio on 7/14/18.
+//  Copyright © 2018 pll. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ StatusBarNotes/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  StatusBarNotes
+//
+//  Created by Filippo Pellolio on 7/14/18.
+//  Copyright © 2018 pll. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
